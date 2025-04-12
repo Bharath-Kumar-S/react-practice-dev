c9c5a932f80d19cbdea77db58dc389872927134b
Bharath Kumar <bharathkumarkarthick@gmail.com>
2025-04-12 17:02:55 +0300

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h3 className="text-sky-700 text-2xl font-bold underline cursor-text text-center">
+      <h3 className="text-sky-700 text-2xl font-bold underline cursor-text text-center m-5">
         Tailwind !!!
       </h3>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
     </>
   );
 }
